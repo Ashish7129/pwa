d1731a14ae0255d1f25b524e55b619e1c5a6c450
Vikram Bishnoi <vikram.bishnoi@nagarro.com>
2019-04-04 00:19:49 +0530

    changes

--- sw.js
+++ sw.js
@@ -1,18 +1,18 @@
-/*self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('video-store').then(function(cache) {
-     return cache.addAll([
-       '/pwa-examples/a2hs/',
-       '/pwa-examples/a2hs/index.html',
-       '/pwa-examples/a2hs/index.js',
-       '/pwa-examples/a2hs/style.css',
-       '/pwa-examples/a2hs/images/fox1.jpg',
-       '/pwa-examples/a2hs/images/fox2.jpg',
-       '/pwa-examples/a2hs/images/fox3.jpg',
-       '/pwa-examples/a2hs/images/fox4.jpg'
-     ]);
-   })
- );
+/*self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open("video-store").then(function(cache) {
+      return cache.addAll([
+        "/pwa-examples/a2hs/",
+        "/pwa-examples/a2hs/index.html",
+        "/pwa-examples/a2hs/index.js",
+        "/pwa-examples/a2hs/style.css",
+        "/pwa-examples/a2hs/images/fox1.jpg",
+        "/pwa-examples/a2hs/images/fox2.jpg",
+        "/pwa-examples/a2hs/images/fox3.jpg",
+        "/pwa-examples/a2hs/images/fox4.jpg"
+      ]);
+    })
+  );
 });
 */
 self.addEventListener("fetch", function(e) {
